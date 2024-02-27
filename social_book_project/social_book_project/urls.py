27a4a803f4bdf0ff9287a718ae45ca863ddd6199
bansodevaibhav032 <bansodevaibhav032@gmail.com>
2024-02-27 16:33:27 +0530

    Sign-up and Login Functionality added

--- social_book_project/social_book_project/urls.py
+++ social_book_project/social_book_project/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# social_book_project/urls.py
 from django.contrib import admin
 from django.urls import path
+from registration import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logoutpage, name='logout'),
 ]
+
