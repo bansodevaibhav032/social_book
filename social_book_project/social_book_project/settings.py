11481b5eb00bbfdf52197194d857e07d8aedb0f0
bansodevaibhav032 <bansodevaibhav032@gmail.com>
2024-03-01 17:58:14 +0530

    Added Postgres Database

--- social_book_project/social_book_project/settings.py
+++ social_book_project/social_book_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import sqlalchemy
+from sqlalchemy import create_engine
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,12 +81,27 @@ WSGI_APPLICATION = 'social_book_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# Configure SQLAlchemy engine
+# engine = create_engine('postgresql://your_db_user:your_db_password@localhost:5432/your_db_name')
 
 
 # Password validation
